c3d51e301cf6a05ae5314168cb25126536ce3b5a
Justina Xie <Justina@JustinatekiAir.wireless.yale.internal>
2018-09-13 18:10:39 -0400

    Revised Version for HW1

--- R/linear-models.r
+++ R/linear-models.r
@@ -11,5 +11,35 @@
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  lm(formula, data)
+  #Generate the design matrix of X
+  X<-model.matrix(formula, data)
+  
+  #Find y from the formula
+  y<-data[,all.vars(formula)[1]]
+  
+  #Apply QR method to find beta and change beta to the same form as in lm
+  beta<-qr.solve(X,y)
+  beta[which(beta==0)] <- NA
+  
+  #Initiate a model_fit list which has the same class characteristics as lm
+  model_fit<-list()
+  model_fit$coefficients <- beta
+  model_fit$residuals<- y-X%*%beta
+  model_fit$fitted.values<-X%*%beta
+  model_fit$rank <- ncol(X)
+  model_fit$weights <- NULL
+  model_fit$df.residuals <- nrow(X) - ncol(X) 
+  model_fit$call <- call('lm',formula)
+  model_fit$terms <- terms(x = formula,data = data)
+  model_fit$contrasts <- NULL
+  model_fit$xlevels <- NULL
+  model_fit$offset <-NULL
+  model_fit$y <- y
+  model_fit$x <- X
+  model_fit$model = formula
+  model_fit$na.action <- NULL
+  model_fit$qr <- qr(X)
+
+  class(model_fit)<-"lm"
+  return(model_fit)
 }
